7031ea3d33f381cfd031bdf876bbe248352ce32b
pcampbelldawson <pcampbell.dawson@gmail.com>
2016-03-07 23:56:35 +0000

    fix phone regex

--- 09-regex-bash/regex-phone.sh
+++ 09-regex-bash/regex-phone.sh
@@ -7,20 +7,24 @@ if [ $#  -eq 0 ] ; then
    echo $(basename $0 ): need a cli arg
    exit
 fi
-# basic regex  no shortcuts
-if [[ "$*" =~  ^\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}$ ]] ; then
+# basic regex 
+echo -n "basic regex: "
+if [[ "$1" =~  ^\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}$ ]] ; then
     echo "North american phone number (999)-999-9999 or 999-999-9999"
 else 
     echo not North American $1
 fi 
 # posix regex
+echo -n "Posix regex: "
 if [[ "$1" =~  ^\(?[[:digit:]]{3}\)?-[[:digit:]]{3}-[[:digit:]]{4}$ ]] ; then
-    echo "North american phone number (999)-999-9999 or 999-999-999"
+    echo "North american phone number (999)-999-9999 or 999-999-9999"
 else 
     echo not North American $1
 fi 
-# shortcuts regex
-if [[ "$1" =~  ^\(?\d{3}\)?-[[:digit:]]{3}-[[:digit:]]{4}$ ]] ; then
+echo "This won't work in current versions of bash"
+echo -n "Shortcuts: " 
+# shortcuts regex  (do not work in later versions of bash  (nor gnu sed))  :(
+if [[ "$1" =~  ^\(?\d{3}\)?-\d{3}-\d{4}$ ]] ; then
     echo "North american phone number (999)-999-9999 or 999-999-999"
 else 
     echo not North American $1
