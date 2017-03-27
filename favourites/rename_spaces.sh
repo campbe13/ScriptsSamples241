525597bed6b2c136785bd71d672577714ee3122d
P Campbell <triciamontreal@gmail.com>
2017-03-27 14:25:45 -0400

    fixed some errors with lots of files
    could not rename lots of long names so I changed it to read in a file which contains a list of names

--- favourites/rename_spaces.sh
+++ favourites/rename_spaces.sh
@@ -1,13 +1,15 @@
-!/bin/bash
+#!/bin/bash
 # PMCampbell
 # rename files input
 # change spaces to underscorses
 if [[ $# -le 0 ]] ; then
-    echo $(basename) Rename file or file list, change blanks to underscores
-    echo $(basename) file1 file2 file3...
+    echo $(basename $0) Rename file list, change blanks to underscores
+    echo $(basename $0 ) filename
     exit 1
 fi
-ls $* | while read -r fn
+while  read -r fn
 do
-    mv -v "$fn" `echo $fn | tr ' ' '_' `
-done
+    echo $fn
+    newfn=$(echo $fn | tr ' ' '_' )
+    mv -v "$fn" $newfn 
+done < $1
